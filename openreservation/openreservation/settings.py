d2a698369242462dde312b359a0ef554b9263c8c
chargio <sergio.ocon@redhat.com>
2020-07-19 13:53:37 +0200

    adding WhiteNoiseMiddleware

--- openreservation/openreservation/settings.py
+++ openreservation/openreservation/settings.py
@@ -69,6 +69,7 @@ SITE_ID = 11
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
